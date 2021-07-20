152f7178fdf2a8c73f7caf822dd6039efe6c0cc7
Joshua Lokken <joshualokken@pm.me>
2021-07-20 14:21:57 -0500

    Adding tests

--- tests/integration/components/share-button-test.js
+++ tests/integration/components/share-button-test.js
@@ -1,26 +1,48 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import Service from '@ember/service';
+import { find, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+
+class MockRouterService extends Service {
+  get currentURL() {
+    return '/foo/bar?baz=true#some-section';
+  }
+}
 
 module('Integration | Component | share-button', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function () {
+    this.owner.register('service:router', MockRouterService);
 
-    await render(hbs`<ShareButton />`);
-
-    assert.dom(this.element).hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <ShareButton>
-        template block text
-      </ShareButton>
-    `);
-
-    assert.dom(this.element).hasText('template block text');
+    this.tweetParam = (param) => {
+      let link = find('a');
+      let url = new URL(link.href);
+      return url.searchParams.get(param);
+    };
   });
+
+  test('basic usage', async function (assert) {
+    await render(hbs`<ShareButton>Tweet this!</ShareButton>`);
+
+    assert.equal(
+      this.tweetParam('url'),
+      new URL('/foo/bar?baz=true#some-section', window.location.origin)
+    );
+  });
+
+  test('it supports passing @text', async function (assert) {
+    await render(
+      hbs`<ShareButton @text="Hello Twitter!">Tweet this!</ShareButton>`
+    );
+  });
+  assert
+    .dom('a')
+    .hasAttribute('target', '_blank')
+    .hasAttribute('rel', 'external nofollow noopener noreferrer')
+    .hasAttribute('href', /^https:\/\/twitter\.com\/intent\/tweet/)
+    .hasClass('share')
+    .hasClass('button')
+    .containsText('Tweet this!');
 });
