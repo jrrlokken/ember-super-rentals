7117b154dfdab3168d7f3acf1711c0f8b28e2bcf
Joshua Lokken <joshualokken@pm.me>
2021-07-21 10:28:21 -0500

    Finished app

--- tests/integration/components/share-button-test.js
+++ tests/integration/components/share-button-test.js
@@ -26,6 +26,15 @@ module('Integration | Component | share-button', function (hooks) {
   test('basic usage', async function (assert) {
     await render(hbs`<ShareButton>Tweet this!</ShareButton>`);
 
+    assert
+      .dom('a')
+      .hasAttribute('target', '_blank')
+      .hasAttribute('rel', 'external nofollow noopener noreferrer')
+      .hasAttribute('href', /^https:\/\/twitter\.com\/intent\/tweet/)
+      .hasClass('share')
+      .hasClass('button')
+      .containsText('Tweet this!');
+
     assert.equal(
       this.tweetParam('url'),
       new URL('/foo/bar?baz=true#some-section', window.location.origin)
@@ -36,13 +45,46 @@ module('Integration | Component | share-button', function (hooks) {
     await render(
       hbs`<ShareButton @text="Hello Twitter!">Tweet this!</ShareButton>`
     );
+
+    assert.equal(this.tweetParam('text'), 'Hello Twitter!');
   });
-  assert
-    .dom('a')
-    .hasAttribute('target', '_blank')
-    .hasAttribute('rel', 'external nofollow noopener noreferrer')
-    .hasAttribute('href', /^https:\/\/twitter\.com\/intent\/tweet/)
-    .hasClass('share')
-    .hasClass('button')
-    .containsText('Tweet this!');
+
+  test('it supports passing @hashtags', async function (assert) {
+    await render(
+      hbs`<ShareButton @hashtags="foo,bar,baz">Tweet this!</ShareButton>`
+    );
+
+    assert.equal(this.tweetParam('hashtags'), 'foo,bar,baz');
+  });
+
+  test('it supports passing @via', async function (assert) {
+    await render(hbs`<ShareButton @via="emberjs">Tweet this!</ShareButton>`);
+
+    assert.equal(this.tweetParam('via'), 'emberjs');
+  });
+
+  test('it supports adding extra classes', async function (assert) {
+    await render(
+      hbs`<ShareButton class="extra things">Tweet this!</ShareButton>`
+    );
+
+    assert
+      .dom('a')
+      .hasClass('share')
+      .hasClass('button')
+      .hasClass('extra')
+      .hasClass('things');
+  });
+
+  test('the target, rel and href attributes cannot be overridden', async function (assert) {
+    await render(
+      hbs`<ShareButton target="_self" rel="" href="/">Not a Tweet!</ShareButton>`
+    );
+
+    assert
+      .dom('a')
+      .hasAttribute('target', '_blank')
+      .hasAttribute('rel', 'external nofollow noopener noreferrer')
+      .hasAttribute('href', /^https:\/\/twitter\.com\/intent\/tweet/);
+  });
 });
